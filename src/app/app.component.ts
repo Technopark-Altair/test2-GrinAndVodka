aaa6c86b3936b3f0b64f3cb2da2ed6a623bfa6b2
Kostya <tereshkoffd80@gmail.com>
2020-10-26 22:57:21 +0300

    Homework-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { getLocaleExtraDayPeriodRules } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lvl-app';
+  firstOp:number;
+  secondOp:number;
+  op:string;
+  result:number;
+  errorMes:string;
+  
+  doCalc() {
+  if (this.firstOp != null && this.secondOp != null && this.op != null) {
+      if (this.op == "-") {
+        this.result = this.firstOp - this.secondOp;
+        this.errorMes = "Ошибок нет";
+      } else if (this.op =="+") { 
+        this.result = this.firstOp + this.secondOp;
+        this.errorMes = "Ошибок нет";
+      } else if(this.op =="/") {
+        if (this.secondOp == 0) {
+          this.errorMes ="На ноль нельзя делить";
+          this.result == null;
+        }else {
+          this.result = this.firstOp / this.secondOp;
+          this.errorMes = "Ошибок нет";
+        }
+      } else if(this.op =="*") {
+        this.result = this.firstOp * this.secondOp;
+        this.errorMes = "Ошибок нет";
+      }
+    }else{
+      if(this.firstOp == null){
+        this.errorMes =" Нет 1 операнда";
+      }else if (this.secondOp == null){
+        this.errorMes = "Нет 2 операнда";
+      }else if (this.op == null){
+        this.errorMes = "Нет оператора действия";
+      }
+    }
+  }
 }
